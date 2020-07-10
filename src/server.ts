a0f420aeed2039e0b077277f8d2b0fffea9ad7ee
Anupam Kumar <54744326+er-anupam-kumar@users.noreply.github.com>
2020-07-10 20:04:29 +0530

    Change of port

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ export class Server {
 
   private activeSockets: string[] = [];
 
-  private readonly DEFAULT_PORT = 5000;
+  private readonly DEFAULT_PORT = process.env.PORT || 3000;
 
   constructor() {
     this.initialize();
@@ -89,7 +89,7 @@ export class Server {
     });
   }
 
-  public listen(callback: (port: number) => void): void {
+  public listen(callback: (port) => void): void {
     this.httpServer.listen(this.DEFAULT_PORT, () => {
       callback(this.DEFAULT_PORT);
     });
